ff40100757bbcda732e58cfe4455edcc0e2cd6ae
Mateo2119 <31406793+Mateo2119@users.noreply.github.com>
2020-03-09 17:30:47 -0500

    Update vJjrcvzk (2).pl
    adasdasd

--- vJjrcvzk (2).pl
+++ vJjrcvzk (2).pl
@@ -1,49 +1,14 @@
-% Autor:
-% Fecha: 9/03/2020
-%personajes
-enemigo('nave_enemiga').
-jugador('nave_jugador').
-%balas
-bala('livianas').
-bala('pesadas').
-%ataque
-dispara('bala').
-impacta(jugador,enemigo).
-impacta(enemigo,jugador).
-%puntos
-puntos('tiempo').
-vidas(1)
-vidas(2)
-vidas(3)
-regenera('puntos,vida').
-%movimiento
-direccion_nave('arriba').
-direccion_nave('abajo').
-direccion_nave('izquierda').
-direccion_nave('derecha').
-velocidad_nave('rapido').
-velocidad_nave('normal').
+%piedra1,tijera2papel3
+gana(1,2).
+gana(3,1).
+gana(2,3).
 
-%nivel
-primernivel(1).
-ultimonivel(3).
-%puntuacion
-puntacionminima(0).
-puntuacionmaxima(0).
-%Tiempo
-tiempo_inicial(10000).
-tiempo_final(0).
-%final
-%reglas
+empate(2,2).
+empate(1,1).
+empate(3,3).
 
+mano(1).
+mano(2).
+mano(3).
 
-daño(X,Y):-dispara(A),impacta(X,Y).
-daño(Y,X):- dispara(A),impacta(Y,X).
-pierdevida(X):-daño(X,Y). 
-muere(X):-vidas(1),pierdevida(X).
-termina(X,Y):-tiempo(0).
-finaljuego(Y):-muere(X,Y);termina(A,Y).
-regenera:-vidas=<2,puntuacion==(2000).    
-
-
-
+gana1(X,C):-mano(X),random(1,4,C),gana(X,C).
